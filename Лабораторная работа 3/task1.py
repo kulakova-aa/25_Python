8ccd910fe0f8b7b856d959a3f1e31e4719f3a0da
kulakova-aa <kulakova.aa@edu.spbstu.ru>
2026-01-08 16:44:43 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,16 @@
+# TODO Напишите функцию для поиска индекса товара
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+def find_first_occurrence(items_list, item_to_find):
+    for index, item in enumerate(items_list):
+        if item == item_to_find:
+            return index
+    return None
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_first_occurrence(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
